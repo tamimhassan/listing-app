f3484e2206838529b4a4c541f250b0ef6878dad3
tamimhassan <tamimhassan506@gmail.com>
2021-04-07 23:07:13 +0600

    initiate react-native app

---
+++ ApartmentRentalApp/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to Home Page</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
